daa5243e8cd58dc4ab9d2a9af94ab54bea52068d
+ <+>
2023-04-17 13:05:59 +0300

    adding search icon

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,5 +1,5 @@
 
-// import { BiSearchAlt2 } from "react-icons/bi"
+import { BiSearchAlt2 } from "react-icons/bi"
 
 function Search(){
 
@@ -8,9 +8,9 @@ function Search(){
         // className='flex flex-row items-center'
         >
             <input type="text" placeholder="Search recipe" className="border border-black rounded-lg p-2"/>
-            {/* <div className="py-2.5 mr-1 px-1 rounded-lg text-3xl ">
+            <div className="py-2.5 mr-1 px-1 rounded-lg text-3xl ">
                 <BiSearchAlt2/>
-            </div> */}
+            </div>
         </div>
     )
 }
