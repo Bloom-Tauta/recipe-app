7df5ea2d1bd785528e102b35f4ac76be2298e934
+ <+>
2023-04-17 13:05:22 +0300

    styling  Search

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -7,8 +7,8 @@ function Search(){
         <div
         // className='flex flex-row items-center'
         >
-            {/* <input type="text" placeholder="Search recipe" className="border border-black rounded-lg p-2"/>
-            <div className="py-2.5 mr-1 px-1 rounded-lg text-3xl ">
+            <input type="text" placeholder="Search recipe" className="border border-black rounded-lg p-2"/>
+            {/* <div className="py-2.5 mr-1 px-1 rounded-lg text-3xl ">
                 <BiSearchAlt2/>
             </div> */}
         </div>
