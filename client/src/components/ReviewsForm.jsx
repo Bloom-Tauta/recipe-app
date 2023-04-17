d9d8e685c29c5ba488c8c394130fbfdd0f04735f
+ <+>
2023-04-17 14:43:22 +0300

    onSubmit on cancel and submit buttons

--- client/src/components/ReviewsForm.jsx
+++ client/src/components/ReviewsForm.jsx
@@ -34,6 +34,10 @@ function ReviewsForm(){
         setComment(event.target.value);
       };
 
+      function handleSubmit(e){
+        e.preventDefault();
+      }
+
     return(
         <div>
             <h3 className='text-center '>Your ratings</h3>
@@ -72,10 +76,10 @@ function ReviewsForm(){
       </div>
       <div className="flex flex-row items-center gap-3 justify-end mt-4">
           <input type="submit" value="Cancel"
-        //   onClick={handleSubmit}
+          onClick={handleSubmit}
           className="text-blue-500 hover:underline"/>
           <button type="submit"
-        // onClick={handleSubmit}
+        onClick={handleSubmit}
           className="border border-black p-2 bg-green-100 hover:bg-green-800">Submit</button>
         </div>
         </div>
