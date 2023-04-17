1826e64c2a8b9a7e6f2bd2dfa39eedb805d684cd
+ <+>
2023-04-17 13:41:44 +0300

    Recipe component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import RecipeForm from './components/RecipeForm';
 
 function App() {
   return (
     <div className="App">
-
+      <RecipeForm/>
     </div>
   );
 }
---
+++ client/src/components/RecipeForm.jsx
@@ -0,0 +1,7 @@
+
+function RecipeForm(){
+    return(
+        <div></div>
+    )
+}
+export default RecipeForm;
