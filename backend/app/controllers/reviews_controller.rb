aa598d3ca2779241202e02ecfa7b621ff5dc9991
Stellamargy <stella.oloo@student.moringaschool.com>
2023-04-18 19:30:07 +0300

    added show ,index actions for review resource

--- backend/app/controllers/reviews_controller.rb
+++ backend/app/controllers/reviews_controller.rb
@@ -1,5 +1,16 @@
 class ReviewsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    def index
+        reviews=Review.all 
+        render json: reviews, status:200
+
+    end
+    def show
+        review=find_review
+        render json: review
+    end
     def create
         review=Review.create!(reviews_params)
         render json: review, status: :created
@@ -7,6 +18,9 @@ class ReviewsController < ApplicationController
     end
     private 
     # strong params
+    def find_review
+        Review.find(params[:id])
+    end
     def reviews_params
         params.permit(:ratings,:comments,:recipe_id,:user_id)
 
@@ -14,4 +28,7 @@ class ReviewsController < ApplicationController
     def render_unprocessable_entity_response(invalid)
         render json: { errors: invalid.record.errors }, status: :unprocessable_entity
     end
+    def render_not_found_response
+        render json: { error: "Review not found" }, status: :not_found
+      end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :reviews, only:[:create]
+  resources :reviews, only:[:create, :show, :index]
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
