49808aba4c8eeb006db70366622682974cae6239
Stellamargy <stella.oloo@student.moringaschool.com>
2023-04-19 23:07:00 +0300

    configured app to use active storage

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_15_140854) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_19_141413) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,36 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_15_140854) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "country_of_origin"
+    t.string "number_of_people_served"
+    t.text "ingredients"
+    t.text "instructions"
+    t.datetime "datetime"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "recipe_image"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "ratings"
+    t.text "comments"
+    t.integer "recipe_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
