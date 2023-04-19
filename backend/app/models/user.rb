beeb8ef0ab60416822594e5739eb69008feacd12
leah wanjiku <leagm22@gmail.com>
2023-04-19 22:29:51 +0300

    intergrated sendgrid

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
 validates_format_of :email, with: /@/
-validates :email, presence: true, uniqueness:  true
+validates :email, presence: true 
+# , uniqueness:  true
 validates :username, uniqueness: { case_sensitive: false }
 validates :password, length: { in: 6..10 }
    
