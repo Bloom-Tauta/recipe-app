378667f62591174ef17acdc982bacc0fa88cfbab
Stellamargy <stella.oloo@student.moringaschool.com>
2023-04-20 00:32:46 +0300

    defined routes for recipe resource

--- backend/app/models/recipe.rb
+++ backend/app/models/recipe.rb
@@ -3,4 +3,7 @@ class Recipe < ApplicationRecord
   belongs_to :user
   belongs_to :admin
   has_one_attached :recipe_image
+  def image_url
+    Rails.application.routes.url_helpers.url_for(recipe_image) if recipe_image.attached?
+  end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users
+  resources :recipes, only:[:index, :show, :update, :destroy, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
