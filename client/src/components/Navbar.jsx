3d2fda6a8ca1dd49451806bc2390fc6caff032f7
Bloom <bloomtauta@gmail.com>
2023-04-23 13:32:46 +0300

    folder changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,15 @@
-import { NavLink } from "react-router-dom";
-import Search from "./Search";
+import { useState } from 'react';
+import { NavLink  } from 'react-router-dom';
+import Searchbar from './Searchbar'
 
+function Navbar(){
 
-function NavBar(){
+    const [search, setSearch] = useState('')
+
     return(
         <div className="bg-gray-700 p-4 items-center justify-between flex ">
             <span className="text-white text-4xl">Recipe-Share</span>
-            <Search/>
+            <Searchbar search={search} setSearch={setSearch} />
             <div>
                 <ul className=" flex gap-4 text-blue-300 text-base ">
                     <NavLink to="/" className="hover:text-blue-800 font-bold" >Home</NavLink>
@@ -19,6 +22,4 @@ function NavBar(){
     )
 }
 
-export default NavBar;
-
-
+export default Navbar;
