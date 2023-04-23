2302493dcbeee413f9aa6856abfc28dd16fbcdca
+ <+>
2023-04-24 02:10:49 +0300

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import { Route, Routes } from "react-router-dom"
 import React,{ useState} from 'react';
 import LandingPage from './components/LandingPage';
-import NavBar from './components/NavBar';
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 // import TheDetails from './components/TheDetails';
-import UserDashboard from './components/UserDashboard';
-import RecipePage from './components/RecipePage';
+// import UserDashboard from './components/UserDashboard';
+// import RecipePage from './components/RecipePage';
 import Share from './components/Share';
 import ReviewsForm from './components/ReviewsForm';
 import RecipeDetailPage from './components/RecipeDetailPage';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 function App() {
 
@@ -23,14 +25,16 @@ function App() {
 
   return (
     <div className="App">
-      <NavBar search={search} handleSearch={handleSearch} />
+      <Navbar search={search} handleSearch={handleSearch} />
       <div className='min-h-[70vh]'>
       <Routes>
         <Route path="/" element={<LandingPage search={search}/>}/>
-        <Route path="/viewmeals/:id" element={<RecipePage/>}/>
-        <Route path="/user-dashboard" element={<UserDashboard/>}/>
+        {/* <Route path="/viewmeals/:id" element={<RecipePage/>}/> */}
+        {/* <Route path="/user-dashboard" element={<UserDashboard/>}/> */}
         <Route path="/viewmeal/:id" element={<RecipeDetailPage />}/>
         <Route path="/share" element={<Share/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/login" element={<Login/>}/>
         <Route path="/reviews" element={<ReviewsForm/>}/>
       </Routes>
       </div>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { NavLink  } from 'react-router-dom';
-import Searchbar from './Searchbar'
+import Search from './Search'
 //import './navbar.css';
 
 function Navbar(){
@@ -10,7 +10,7 @@ function Navbar(){
     return(
         <div className="bg-gray-700 p-4 items-center justify-between flex">
             <span className="text-white text-4xl">Recipe-Share</span>
-            <Searchbar search={search} setSearch={setSearch} />
+            <Search search={search} setSearch={setSearch} />
             <div>
                 <ul className="flex gap-4 text-blue-300 text-base ">
                     <NavLink to="/" className="hover:text-blue-800 font-bold" >Home</NavLink>
@@ -20,7 +20,7 @@ function Navbar(){
                 </ul>
             </div>
         </div>
-        
+
     )
 }
 
