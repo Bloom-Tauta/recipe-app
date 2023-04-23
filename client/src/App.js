6ad1a4f51db03a6911ff726cbf8bd1f9ce9d80cc
Bloom <bloomtauta@gmail.com>
2023-04-23 15:10:14 +0300

    app.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,56 @@ import Login from './components/Login';
 
 
 function App() {
-  return(
-    <About/>
-  )
+  // function handleSubmit (e){
+  //   e.preventDefault()
+  //   const formData=new FormData()
+  //   formData.append("recipe[recipeName]",e.target.recipeName.value)
+  //   formData.append("recipe[recipeimage]",e.target.recipeImage.files[0])
+  //   formData.append("recipe[description]",e.target.description.value)
+  //   formData.append("recipe[countryOfOrigin]",e.target.countryOfOrigin.value)
+  //   formData.append("recipe[numberOfPeopleServed]",e.target.numberOfPeopleServed.value)
+  //   formData.append("recipe[ingredients]",e.target.ingredients.value)
+  //   formData.append("recipe[instructions]",e.target.instructions.value)
+  //   formData.append("recipe[dateTime]",e.target.dateTime.value)
+  //    sendDataToBackEnd(formData)
+  //   console.log(e.target.image.files[0])
+  //   console.log(e.target.title.value)
+  //   console.log(e.target.description.value)
+  // }
+  // function sendDataToBackEnd(formData){
+  //   fetch("http://localhost:3000/recipes",{
+  //     method: "POST",
+  //     body: formData
+  //   }).then(res=> res.json())
+  //   .then(data=>{
+  //     console.log(data)
+  //   })
+  // }
+  return (
+    // <div className="App">
+    //   { <form onSubmit={(e)=>handleSubmit(e)}>
+    //     <input placeholder='recipe-name' type="text" name="recipe-name" />
+    //     <input type="file" name="recipe-image" accept="image/jpg, image/png" />
+    //     <input placeholder='description' type="text" name="description" />
+    //     <input placeholder='countrty-of -origin' type="text" name="country-of-origin" />
+    //     <input placeholder='number-of-people-served' type="text" name="number-of-people served" />
+    //     <input placeholder='ingredients' type="text" name="ingredients" />
+    //     <input placeholder='instructions' type="text" name="instructions" />
+    //     <input placeholder='datetime' type="text" name="datetime" />
+
+    //     <input type="submit"  />
+    //   </form> }
+      <Routes>
+        <Route path="/" element={<LandingPage/>}/>
+        <Route path="/recipe-form" element={<RecipeForm/>}/>
+        <Route path="/rating" element={<RatingForm/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/about" element={<About/>}/>
+
+      </Routes>
+
+    // </div>
+  );
 }
 export default App;
