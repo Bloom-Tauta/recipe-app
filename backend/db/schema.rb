7b23f79ea75a8fa657706edf6b6db1685f543d89
Bloom <bloomtauta@gmail.com>
2023-04-23 14:10:11 +0300

    new db

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_23_105902) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_23_110611) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,21 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_23_105902) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "recipe_name"
+    t.text "description"
+    t.string "country_of_origin"
+    t.string "number_of_people_served"
+    t.text "ingredients"
+    t.text "instructions"
+    t.datetime "date_time"
+    t.integer "user_id"
+    t.integer "admin_id"
+    t.boolean "approved", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "reviews", force: :cascade do |t|
