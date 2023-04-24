afed86a93c45d59f5e7b9f9394b6810dc887e36d
+ <+>
2023-04-24 03:35:14 +0300

    filter

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,27 +1,24 @@
-import { useState } from 'react';
-import { NavLink  } from 'react-router-dom';
-import Search from './Search'
-//import './navbar.css';
+import { NavLink } from "react-router-dom";
+import Search from "./Search";
 
-function Navbar(){
 
-    const [search, setSearch] = useState('')
-
+function Navbar({search, handleSearch}){
     return(
-        <div className="bg-gray-700 p-4 items-center justify-between flex">
+        <div className="bg-gray-700 p-4 items-center justify-between flex ">
             <span className="text-white text-4xl">Recipe-Share</span>
-            <Search search={search} setSearch={setSearch} />
+            <Search search={search} handleSearch={handleSearch} />
             <div>
-                <ul className="flex gap-4 text-blue-300 text-base ">
+                <ul className=" flex gap-4 text-blue-300 text-base ">
                     <NavLink to="/" className="hover:text-blue-800 font-bold" >Home</NavLink>
-                    <NavLink to="/signup"  className="hover:text-blue-800 font-bold" >Sign Up</NavLink>
+                    <NavLink to="/sign-up"  className="hover:text-blue-800 font-bold" >Sign Up</NavLink>
                     <NavLink to="/login"  className="hover:text-blue-800 font-bold" >Login</NavLink>
                     <NavLink to="/favorite-recipes"  className="hover:text-blue-800 font-bold" >Favorite Recipes</NavLink>
                 </ul>
             </div>
         </div>
-
     )
 }
 
 export default Navbar;
+
+
--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,22 +1,21 @@
-import { FiSearch } from 'react-icons/fi'
 
-function Search({search,setSearch}){
+import { BiSearchAlt2 } from "react-icons/bi"
+
+function Search({search, handleSearch}){
 
     return(
-    <div className="border border-black rounded-md w-max mx-auto my-8">
-        <div className="searchBar">
-            <div className="flex ">
-                <input
-                value={search}
-                onChange={(e)=> setSearch(e.target.value)}
-                className='bg-gray-300 p-1 outline-none'
-                type="text"
-                placeholder="Search a Recipe"
-                />
-                <span className ='bg-gray-300 flex items-center p-1 text-xl'><FiSearch /></span>
+        <div className='flex items-center'>
+            <div>
+                <div className="flex">
+                    <input value={search} onChange={(e) => handleSearch(e.target.value)}
+                     type="text" placeholder="Search recipe"
+                    className="border border-black rounded-lg p-2 outline-none"/>
+                    <span className="py-2.5 mr-1 px-1 rounded-lg text-3xl ">
+                        <BiSearchAlt2/>
+                    </span>
+                </div>
             </div>
         </div>
-    </div>
     )
 }
 export default Search;
