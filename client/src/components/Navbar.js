2c1c5ecb01a1b6afce6f50a7b063e6c0f37bd5b0
Bloom <bloomtauta@gmail.com>
2023-04-29 16:03:27 +0300

    sendgrid and signup

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -13,7 +13,7 @@ function Navbar(){
             <div>
                 <ul className=" flex gap-4 text-blue-300 text-base ">
                     <NavLink to="/" className="hover:text-blue-800 font-bold" >Home</NavLink>
-                    <NavLink to="/sign-up"  className="hover:text-blue-800 font-bold" >Sign Up</NavLink>
+                    <NavLink to="/signup"  className="hover:text-blue-800 font-bold" >Sign Up</NavLink>
                     <NavLink to="/login"  className="hover:text-blue-800 font-bold" >Login</NavLink>
                     <NavLink to="/favorite-recipes"  className="hover:text-blue-800 font-bold" >Favorite Recipes</NavLink>
                 </ul>
