470330ca0f83f4c22721cffe8c6bd395635f4f7a
Bloom <bloomtauta@gmail.com>
2023-09-11 18:11:00 +0300

    done

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -127,3 +127,50 @@ def authorized
   render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
 end
 end
+
+# app/controllers/application_controller.rb
+
+class ApplicationController < ActionController::API
+  include JwtToken
+
+  before_action :authenticate_customer
+
+  private
+
+  def authenticate_customer
+    header = request.headers['Authorization']
+    if header && header.start_with?('Bearer ')
+      token = header.split(' ').last
+
+      begin
+        @decoded = JwtToken.decode(token)
+        @current_customer = Customer.find(@decoded[:customer_id])
+      rescue ActiveRecord::RecordNotFound => e
+        render json: { errors: e.message }, status: :unauthorized
+      rescue JWT::DecodeError => e
+        render json: { errors: e.message }, status: :unauthorized
+      end
+    else
+      render json: { errors: 'Authorization header missing or invalid' }, status: :unauthorized
+    end
+  end
+
+  def authenticate_seller
+    header = request.headers['Authorization']
+    if header && header.start_with?('Bearer ')
+      token = header.split(' ').last
+
+      begin
+        @decoded = JwtToken.decode(token)
+        @current_seller = Customer.find(@decoded[:seller_id])
+      rescue ActiveRecord::RecordNotFound => e
+        render json: { errors: e.message }, status: :unauthorized
+      rescue JWT::DecodeError => e
+        render json: { errors: e.message }, status: :unauthorized
+      end
+    else
+      render json: { errors: 'Authorization header missing or invalid' }, status: :unauthorized
+    end
+  end
+  
+end
