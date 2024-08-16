c54ef53c1c5718dcfed220c1ff236db7b20708f4
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-08-16 15:27:06 +0300

    setting root route

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -15,6 +15,6 @@ Rails.application.routes.draw do
   resources :reviews, only: [:create, :index, :destroy, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
-  # root "users#index"
+  root "recipes#index"
 
 end
