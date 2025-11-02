ce0018c641d9224bc16428f0e3c6764c7939ece9
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2025-11-03 00:41:58 +0300

    user controller changes

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -2,10 +2,8 @@ class UsersController < ApplicationController
   # skip_before_action :authorized, only: [:create, :login]
   before_action :current_user
 
-
-
   def index
-    users= User.all
+    users = User.all
     render json: users
   end
 
@@ -19,8 +17,6 @@ class UsersController < ApplicationController
     end
   end
 
-  
-
   def show
     user = User.find(params[:id])
     render json: user, status: :ok
@@ -28,38 +24,34 @@ class UsersController < ApplicationController
 
   def create
     user = User.create!(user_params)
-      from = "leah.wanjiku@student.moringaschool.com"
-      subject = "New User Account"
-      content = "Thank you for registering with us #{user.username}. Your account has been created successfully"
-      # EmailService.call(from: @from, to: @user.email, subject: @subject, content: @content)
-      render json: user, status: :created
+    render json: user, status: :created
   end
 
-    # def login
-    #   user = User.find_by(username: user_params[:username])
-    #   if user && user.authenticate(user_params[:password])
-    #     token = JWT.encode({ username: user.username }, 'my-secret-key', 'HS256')
+  # def login
+  #   user = User.find_by(username: user_params[:username])
+  #   if user && user.authenticate(user_params[:password])
+  #     token = JWT.encode({ username: user.username }, 'my-secret-key', 'HS256')
 
-    #     cookies.signed[:jwt] = { value: token, httponly: true, secure: false }
+  #     cookies.signed[:jwt] = { value: token, httponly: true, secure: false }
 
-    #     render json: user, status: :ok
-    #   else
-    #     render json: { error: 'Invalid username or password' }, status: :unauthorized
-    #   end
-    # end
+  #     render json: user, status: :ok
+  #   else
+  #     render json: { error: 'Invalid username or password' }, status: :unauthorized
+  #   end
+  # end
 
-    # def logout
-    #   cookies.delete(:jwt, httponly: true)
-    #   render json: { message: 'Logged out successfully' }
-    # end
+  # def logout
+  #   cookies.delete(:jwt, httponly: true)
+  #   render json: { message: 'Logged out successfully' }
+  # end
 
-    def me
-      render json: @current_user
-    end
+  def me
+    render json: @current_user
+  end
 
-    private
+  private
 
-    def user_params
-      params.permit(:username, :password, :email, :admin, :favourites)
-    end
+  def user_params
+    params.permit(:username, :password, :email, :admin, :favourites)
+  end
 end
