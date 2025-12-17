70c37190ca9b9005bb37a38373127c0f219685da
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2025-12-17 19:32:40 +0300

    cors and production file

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins "http://localhost:4000"
-      resource "*",
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head]
-    end
+  allow do
+    origins 'https://recipe-app-eight-chi.vercel.app'
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete options head]
   end
+end
